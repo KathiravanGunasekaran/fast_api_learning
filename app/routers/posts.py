ac7d7821d628fba1a1500933ca9e9ba6956d2d4b
G.Kathiravan <Kathirguna0811@gmail.com>
2024-08-04 23:35:20 +0530

    Update posts.py

--- app/routers/posts.py
+++ app/routers/posts.py
@@ -5,19 +5,18 @@ from sqlalchemy.orm import Session
 from typing import List
 from ..schemas import CreatePost, PostResponse
 
-router = APIRouter()
-
+# since all the routes are same expect for those has ID we can use this prefix to define out route
+router = APIRouter(prefix="/api/v1/posts") 
 
 # we are sending list of posts so we are converting the post model to list of post model
-@router.get("/api/v1/posts", response_model=List[PostResponse])
+@router.get("/", response_model=List[PostResponse])
 # we have add this session as parameter to this function every-time
 async def get_posts(db: Session = Depends(get_db)):
     post = db.query(models.Post).all()
     return post
 
-
 # adding the model to the response we are getting from the server that is response_model=<model of the response>
-@router.post("/api/v1/posts", status_code=status.HTTP_201_CREATED, response_model=PostResponse)
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=PostResponse)
 async def create_post(post: CreatePost, db: Session = Depends(get_db)):
     # referring the model that we created in model.py and adding the values in it like how we do in insert into query
     # new_post = models.Post(title=post.title, content=post.content, published=post.published) - one way
@@ -33,8 +32,7 @@ async def create_post(post: CreatePost, db: Session = Depends(get_db)):
 
     return new_post
 
-
-@router.get("/api/v1/posts/{id}", response_model=PostResponse)
+@router.get("/{id}", response_model=PostResponse)
 async def get_post(id: int, db: Session = Depends(get_db)):
     # using filter by to filter based on the key. the key can be any column name
     # db.query(models.Post).filter_by(id=str(id)).one() ---> you can try like this
@@ -58,8 +56,7 @@ async def get_post(id: int, db: Session = Depends(get_db)):
     else:
         raise HTTPException(status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} not found")
 
-
-@router.put("/api/v1/posts/{id}", response_model=PostResponse)
+@router.put("/{id}", response_model=PostResponse)
 async def update_post(id: int, updated_post: CreatePost, db: Session = Depends(get_db)):
     post_query = db.query(models.Post).filter(models.Post.id == id)
     post = post_query.first()
@@ -69,8 +66,7 @@ async def update_post(id: int, updated_post: CreatePost, db: Session = Depends(g
     db.commit()
     return post_query.first()
 
-
-@router.delete("/api/v1/posts/{id}")
+@router.delete("/{id}")
 async def delete_post(id: int, db: Session = Depends(get_db)):
     post = db.query(models.Post).filter(models.Post.id == id)
     if post.first() == None:
