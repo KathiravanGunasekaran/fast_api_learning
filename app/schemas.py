d731f5ae04631309e343f5ba0f96ccfb1a9d463a
G.Kathiravan <Kathirguna0811@gmail.com>
2024-08-04 23:29:07 +0530

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 
 # this is py-dyantic model pr schema and this is referred in path operations to shape the req body and response
@@ -16,7 +16,7 @@ class CreatePost(PostBase):
     pass
 
 
-class Post(PostBase):
+class PostResponse(PostBase):
     id: int
     created_at: datetime
 
@@ -25,6 +25,20 @@ class Post(PostBase):
     it will send us back the sqlalchemy model it's not a dict to make it work we are using this below config
     
     """
+
+    class Config:
+        orm_mode = True
+
+
+class User(BaseModel):
+    email: EmailStr  # email str will validate particularly for valid email format
+    password: str
+
+
+class UserResponse(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
 
     class Config:
         orm_mode = True
