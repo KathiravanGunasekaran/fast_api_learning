98daaf80c3764a1ef362a9e83dacc8fa34daaa13
G.Kathiravan <Kathirguna0811@gmail.com>
2024-08-04 11:31:40 +0530

    Create schemas.py

---
+++ app/schemas.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from pydantic import BaseModel
+
+
+# this is py-dyantic model pr schema and this is referred in path operations to shape the req body and response
+# this is just a validation layer
+
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+
+class CreatePost(PostBase):
+    pass
+
+
+class Post(PostBase):
+    id: int
+    created_at: datetime
+
+    """
+    for example let's take create when we create a post/item 
+    it will send us back the sqlalchemy model it's not a dict to make it work we are using this below config
+    
+    """
+
+    class Config:
+        orm_mode = True
