0c7b76bc27def6609177984afde519954e747807
G.Kathiravan <Kathirguna0811@gmail.com>
2024-08-05 14:44:54 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,13 +1,11 @@
 from sqlalchemy import Column, Integer, String, Boolean, TIMESTAMP
 from sqlalchemy.sql.expression import (
     text,
-)  # text is from sql, you can add a query inside it if you want kind of typecasting
+)  # text is from sql, you can add a query inside it if you want. kind of typecasting
 from .database import Base
-
 
 # this is sql alchemy model to design our table
 # used to create, query and delete the entries in database
-
 
 class Post(Base):
     __tablename__ = "posts"
@@ -17,7 +15,6 @@ class Post(Base):
     content = Column(String, nullable=False)
     published = Column(Boolean, nullable=False, server_default="TRUE")
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text("NOW()"))
-
 
 class User(Base):
     __tablename__ = "users"
