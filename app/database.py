ecc4a4afbd932533234bb356e596c6fd6e3fd7cf
G.Kathiravan <Kathirguna0811@gmail.com>
2024-08-05 14:46:51 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -5,17 +5,12 @@ from urllib.parse import quote
 
 '''
 pgsql server will not be able to parse the special character
-
 Special Characters in URL:
 If your password or username contains special characters (like @, #, :, /, etc.), they need to be URL-encoded.
-
 For example:
-
 Original password: P@ssw0rd!
 URL-encoded: P%40ssw0rd%21
-
 so, we can use "from urllib.parse import quote" library to parse the values used in the url
-
 '''
 
 user = ""
@@ -25,12 +20,8 @@ host = ""
 dbname = ""
 
 database_url = f"postgresql://{user}:{password}@{host}/{dbname}"
-
-
 engine = create_engine(database_url)
-
 session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
 def get_db():
