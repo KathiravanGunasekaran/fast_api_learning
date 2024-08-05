e69677b7141aba487b589b67b553954c7bd9e821
G.Kathiravan <Kathirguna0811@gmail.com>
2024-08-05 14:46:10 +0530

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -2,6 +2,5 @@ from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")  # telling passlib to use bcrypt hashing algorithm
 
-
 def hash(pwd: str):
     return pwd_context.hash(pwd)
