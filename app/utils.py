4a9e3aec4fcd5ba392ec63d829705bde46f777fe
G.Kathiravan <Kathirguna0811@gmail.com>
2024-08-08 14:53:27 +0530

    Updated  utils.py to verify the password

--- app/utils.py
+++ app/utils.py
@@ -2,5 +2,10 @@ from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")  # telling passlib to use bcrypt hashing algorithm
 
+#hashes the password
 def hash(pwd: str):
     return pwd_context.hash(pwd)
+    
+#verify the password from user while logging in with the hashed password in DB
+def verify(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
