8a18fbb181611558f0f0f29ec40174afea1e3a6b
G.Kathiravan <Kathirguna0811@gmail.com>
2024-08-09 12:09:12 +0530

    Updated models.py with foreignkey

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
-from sqlalchemy import Column, Integer, String, Boolean, TIMESTAMP
+from sqlalchemy import Column, Integer, String, Boolean, TIMESTAMP,ForeignKey
 from sqlalchemy.sql.expression import (
     text,
 )  # text is from sql, you can add a query inside it if you want. kind of typecasting
 from .database import Base
+from sqlalchemy.orm import relationship
 
 # this is sql alchemy model to design our table
 # used to create, query and delete the entries in database
@@ -15,6 +16,9 @@ class Post(Base):
     content = Column(String, nullable=False)
     published = Column(Boolean, nullable=False, server_default="TRUE")
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text("NOW()"))
+    owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+
+    owner = relationship("User")
 
 class User(Base):
     __tablename__ = "users"
