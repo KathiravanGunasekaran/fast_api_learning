885bc877d37e0e341a4935517cb6dfd8baa3b37e
G.Kathiravan <Kathirguna0811@gmail.com>
2024-08-09 12:11:00 +0530

    Updated schemas.py for posts

--- app/schemas.py
+++ app/schemas.py
@@ -6,29 +6,6 @@ from pydantic import BaseModel, EmailStr
 # this is just a validation layer
 
 
-class PostBase(BaseModel):
-    title: str
-    content: str
-    published: bool = True
-
-
-class CreatePost(PostBase):
-    pass
-
-
-class PostResponse(PostBase):
-    id: int
-    created_at: datetime
-    """
-    for example let's take create when we create a post/item 
-    it will send us back the sqlalchemy model it's not a dict to make it work we are using this below config
-    
-    """
-
-    class Config:
-        orm_mode = True
-
-
 class User(BaseModel):
     email: EmailStr  # email str will validate particularly for valid email format
     password: str
@@ -46,6 +23,30 @@ class UserResponse(BaseModel):
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
+
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+
+class CreatePost(PostBase):
+    pass
+
+
+class PostResponse(PostBase):
+    id: int
+    created_at: datetime
+    owner_id: int
+    owner: UserResponse
+    """
+    for example let's take create when we create a post/item 
+    it will send us back the sqlalchemy model it's not a dict to make it work we are using this below config
+    """
+
+    class Config:
+        orm_mode = True
 
 
 class Token(BaseModel):
